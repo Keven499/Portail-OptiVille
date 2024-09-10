67ae9ee60c1d6fae1e6eb1b422906e8c0fbfb83e
Keven499 <kevenmichel@outlook.fr>
2024-09-10 19:42:22 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using Portail_OptiVille.Data;
+using Portail_OptiVille.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddDbContext<A2024420517riGr1Eq6Context>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 18)) // Remplacez par une version générique
+    )
+);
 
 var app = builder.Build();
 
