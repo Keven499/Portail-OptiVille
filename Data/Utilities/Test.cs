c78d03f282779d3cb4b98273c4253339271407c0
MariusTestard <tehtar4857@gmail.com>
2024-10-01 18:06:46 -0400

    Update DemandeFournisseur.razor
    Ok, j'avance très bien sur la demande des fournisseurs, je suis content. J'ai créer la page principale qui accueillera tous les formulaires. Je suis en train de cook comme jamais. Je vais continuer après, je prend une pause.

---
+++ Data/Utilities/Test.cs
@@ -0,0 +1,10 @@
+namespace Portail_OptiVille.Data.Utilities
+{
+    /*
+        CECI EST UN TEST. IL A ÉTÉ CONCLUANT ET JE VAIS CONTINUER TOUT À L'HEURE, SI VOUS VOYEZ CE MESSAGE, N'EFFACEZ PAS CETTE PAGE S'IL VOUS PLAÎT
+    */
+    public class FormModel
+    {
+        public string LicenceRBQInfo { get; set; }
+    }
+}
