473056c9253ca04871b3fa2151288c38f2254b90
MariusTestard <tehtar4857@gmail.com>
2024-10-02 23:20:12 -0400

    Update DemandeFournisseur.razor
    Le premier formulaire des 6 est enfin terminer pour la page où nous sommes supposés remplir notre demande et faire la navigation entre les différents formulaires, ça avance très bien.

--- Data/FormModels/LicenceRBQFormModel.cs
+++ Data/FormModels/LicenceRBQFormModel.cs
@@ -5,11 +5,11 @@ namespace Portail_OptiVille.Data.FormModels
     */
     public class LicenceRBQFormModel
     {
-        public string NumeroLicence { get; set; }
+        public string? NumeroLicence { get; set; }
 
-        public string StatutLicence { get; set; }
+        public string? StatutLicence { get; set; }
 
-        public string TypeLicence { get; set; }
+        public string? TypeLicence { get; set; }
 
         public Dictionary<string, bool> SousCategoSelected { get; set; } = new Dictionary<string, bool>();
     }
