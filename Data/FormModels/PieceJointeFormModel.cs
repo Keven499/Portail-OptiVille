18273fdfafe6d51faa00b312c79d82533003cc45
MariusTestard <tehtar4857@gmail.com>
2024-10-10 13:01:53 -0400

    Update PieceJointe
    Pièce jointes est terminé pour de bon et les données sont sauvegardés.

---
+++ Data/FormModels/PieceJointeFormModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portail_OptiVille.Data.FormModels
+{
+    public class PieceJointeFormModel
+    {
+        [FileExtensionsAttribute(Extensions = "doc,docx,pdf,xls,xlsx,jpg,jpeg,png", ErrorMessage = "Format de fichier non valide.")]
+        public IBrowserFile? Fichier { get; set; }
+
+        public List<IBrowserFile> ListFichiers = new List<IBrowserFile>();
+    }
+}
