f501174a0d017c802a2cab2d88d083e14c4e7cff
TheOtterKaz <mae.grenier.01@edu.cegeptr.qc.ca>
2024-10-10 08:31:32 -0400

    Frontend + validations
    Modification font
    Formulaire identification terminée
    Formulaire coordonnées en cours

--- Data/FormModels/FournisseurFormModel.cs
+++ Data/FormModels/FournisseurFormModel.cs
@@ -9,27 +9,27 @@ namespace Portail_OptiVille.Data.FormModels
             PhoneList = new List<TelephoneFormModel>();
         }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "No requis")]
         [RegularExpression(@"^(\d{1,5}[A-Z]?)\s*(½?)$", ErrorMessage = "Format invalide")]
         public string NoEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requise")]
+        [Required(ErrorMessage = "Rue requise")]
         public string RueEntreprise { get; set; }
 
         public string? BureauEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requise")]
+        [Required(ErrorMessage = "Ville requise")]
         public string VilleEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requise")]
+        [Required(ErrorMessage = "Province requise")]
         public string ProvinceEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "CP requis")]
         [StringLength(6, MinimumLength = 6, ErrorMessage = "6 caractères max")]
         [RegularExpression(@"^[A-z]{1}[\d]{1}[A-z]{1}[\d]{1}[A-z]{1}[\d]{1}$", ErrorMessage = "Format invalide")]
         public string CodePostalEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requise")]
+        [Required(ErrorMessage = "Région adm. requise")]
         public string? RegionAdmEntreprise { get; set; }
 
         public string? SiteWebEntreprise { get; set; }
--- Data/FormModels/IdentificationFormModel.cs
+++ Data/FormModels/IdentificationFormModel.cs
@@ -8,20 +8,20 @@ namespace Portail_OptiVille.Data.FormModels
         [StringLength(10, MinimumLength = 10, ErrorMessage = "10 caractères exigés")]
         public string? NEQ { get; set; }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "Nom requis")]
         public string? NomEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "Courriel requis")]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Format invalide")]
         public string? CourrielEntreprise { get; set; }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "Mdp requis")]
         [StringLength(12, MinimumLength = 7, ErrorMessage = "7 à 12 caractères")]
         [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$", ErrorMessage = "Caractères interdits")]
         // IL MANQUE DES VALIDATIONS POUR LE MOT DE PASSE
         public string? MotDePasse { get; set; }
 
-        [Required(ErrorMessage = "Requis")]
+        [Required(ErrorMessage = "Confirmation requise")]
         [MatchField("MotDePasse", ErrorMessage = "Ne correspondent pas")]
         public string? ConfirmationMotDePasse { get; set; }
     }
