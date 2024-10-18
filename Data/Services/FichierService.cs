125303f7b6a172bd2a9160ed56266556e0878937
MariusTestard <tehtar4857@gmail.com>
2024-10-18 17:15:50 -0400

    Update PiecesJointes
    Bon, j'ai réglé le problème de crash avec le InputFile et en passant j'ai fait crash la BD ??

--- Data/Services/FichierService.cs
+++ Data/Services/FichierService.cs
@@ -47,7 +47,6 @@ namespace Portail_OptiVille.Data.Services
                         }
                         using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
                         {
-                            Console.WriteLine($"---------------------------{stream.Name}---------------------------");
                             await fileStream.CopyToAsync(stream);
                         }
                     }
