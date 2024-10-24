c9e2ade051ea6ced91c23334d5e2648c20ebb12d
MariusTestard <tehtar4857@gmail.com>
2024-10-24 10:41:14 -0400

    Update GestionUser
    Bon, je vois qu'il reste pas mal de travail sur cette page. Je vais réfléchir pour la nouvelle table dans la base de données et je vais la créer.

---
+++ Data/Services/GestionUserService.cs
@@ -0,0 +1,38 @@
+﻿using Portail_OptiVille.Data.Models;
+
+namespace Portail_OptiVille.Data.Services
+{
+    public class GestionUserService
+    {
+        private readonly A2024420517riGr1Eq6Context _context;
+
+        public GestionUserService(A2024420517riGr1Eq6Context context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveUser()
+        {
+            // EN FAIT JE CROIS QU'IL VA NOUS FALLOIR UNE SECONDE TABLE QUI DÉTERMINE JUSTE LE RÔLE DES EMPLOYÉS POUR NE PAS SUPPRIMER LES EMPLOYÉS DE LA TABLE, MAIS SEULEMENT LEUR RÔLE
+        }
+
+        public async Task DeleteUser(Employe employe)
+        {
+            var userToDelete = await _context.Employes.FindAsync(employe.Courriel);
+            if (userToDelete != null)
+            {
+                _context.Employes.Remove(userToDelete);
+                await _context.SaveChangesAsync(); 
+            }
+            else
+            {
+                throw new Exception("User not found");
+            }
+        }
+
+        public async Task UpdateUser()
+        {
+            // EN FAIT JE CROIS QU'IL VA NOUS FALLOIR UNE SECONDE TABLE QUI DÉTERMINE JUSTE LE RÔLE DES EMPLOYÉS POUR NE PAS SUPPRIMER LES EMPLOYÉS DE LA TABLE, MAIS SEULEMENT LEUR RÔLE
+        }
+    }
+}
